7c3c67b4f4f8d10e621c41f3e61cd94249fc02ba
programMike <programMike@github.com>
2018-03-18 12:06:30 -0700

    Added Add Products

---
+++ Baked/Controllers/AddProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Baked.Models;
+
+namespace Baked.Controllers
+{
+    public class AddProductController : Controller
+    {
+        // GET: AddProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "ProductName, ProductPrice")]Product formProduct)
+        {
+            BakeryEntities db = new BakeryEntities();
+            Product dbProduct = new Product();
+
+            dbProduct.ProductName = formProduct.ProductName;
+            dbProduct.ProductPrice = formProduct.ProductPrice;
+
+            db.Products.Add(dbProduct);
+            db.SaveChanges();
+
+            return View("Verification");
+        }
+    }
+}
